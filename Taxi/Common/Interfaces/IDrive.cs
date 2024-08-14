e05d5c859903374b4a1c1eef70672b72f197bb87
Guess88 <milan5ivanovic@gmail.com>
2024-08-14 18:30:03 +0200

    Podesavanje servisa

---
+++ Taxi/Common/Interfaces/IDrive.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Interfaces
+{
+    public interface IDrive : IService
+    {
+    }
+}
